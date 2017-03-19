984058fad52b4295238aa885fd82259c2e7ecbe4
krowpu <krowpu@tightmail.com>
2017-03-19 21:45:21 +0500

    Add existing code

---
+++ metadata.rb
@@ -0,0 +1,14 @@
+name 'monit'
+description 'Monit'
+license 'MIT'
+
+maintainer 'krowpu'
+maintainer_email 'krowpu@tightmail.com'
+
+source_url 'https://github.com/krowpu/chef-monit'
+issues_url 'https://github.com/krowpu/chef-monit/issues'
+
+platform 'ubuntu', '= 16.04'
+
+depends 'dist-update'
+depends 'nginx'
---
+++ recipes/default.rb
@@ -0,0 +1,118 @@
+include_recipe 'nginx'
+
+include_recipe 'dist-update'
+
+package 'monit'
+
+service 'monit' do
+  action :nothing
+end
+
+service 'nginx' do
+  action :nothing
+end
+
+cookbook_file '/etc/monit/monitrc' do
+  source 'monitrc.erb'
+  owner 'root'
+  group 'root'
+  mode '0600' # -rw-------
+
+  notifies :restart, 'service[monit]'
+end
+
+cookbook_file '/etc/nginx/snippets/monit.conf' do
+  source 'nginx.conf'
+  owner 'root'
+  group 'root'
+  mode '0644' # -rw-r--r--
+
+  notifies :restart, 'service[nginx]'
+end
+
+template '/etc/monit/conf-available/system' do
+  source 'system.erb'
+  owner 'root'
+  group 'root'
+  mode '0644' # -rw-r--r--
+
+  notifies :restart, 'service[monit]'
+
+  variables name: data['monit']['system_name']
+end
+
+link '/etc/monit/conf-enabled/system' do
+  to '/etc/monit/conf-available/system'
+  owner 'root'
+  group 'root'
+
+  notifies :restart, 'service[monit]'
+end
+
+cookbook_file '/etc/monit/conf-available/rootfs' do
+  source 'rootfs'
+  owner 'root'
+  group 'root'
+  mode '0644' # -rw-r--r--
+
+  notifies :restart, 'service[monit]'
+end
+
+link '/etc/monit/conf-enabled/rootfs' do
+  to '/etc/monit/conf-available/rootfs'
+  owner 'root'
+  group 'root'
+
+  notifies :restart, 'service[monit]'
+end
+
+cookbook_file '/etc/monit/conf-available/sshd' do
+  source 'sshd'
+  owner 'root'
+  group 'root'
+  mode '0644' # -rw-r--r--
+
+  notifies :restart, 'service[monit]'
+end
+
+link '/etc/monit/conf-enabled/sshd' do
+  to '/etc/monit/conf-available/sshd'
+  owner 'root'
+  group 'root'
+
+  notifies :restart, 'service[monit]'
+end
+
+cookbook_file '/etc/monit/conf-available/monit' do
+  source 'monit'
+  owner 'root'
+  group 'root'
+  mode '0644' # -rw-r--r--
+
+  notifies :restart, 'service[monit]'
+end
+
+link '/etc/monit/conf-enabled/monit' do
+  to '/etc/monit/conf-available/monit'
+  owner 'root'
+  group 'root'
+
+  notifies :restart, 'service[monit]'
+end
+
+cookbook_file '/etc/monit/conf-available/nginx' do
+  source 'nginx'
+  owner 'root'
+  group 'root'
+  mode '0644' # -rw-r--r--
+
+  notifies :restart, 'service[monit]'
+end
+
+link '/etc/monit/conf-enabled/nginx' do
+  to '/etc/monit/conf-available/nginx'
+  owner 'root'
+  group 'root'
+
+  notifies :restart, 'service[monit]'
+end
